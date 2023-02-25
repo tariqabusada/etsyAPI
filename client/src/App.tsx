3994ef44f4df54f8b099687fb5d184a4228be93a
tariqabusada <tariqesa101@gmail.com>
2023-02-26 01:41:46 +0200

    working ...

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,22 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import {useEffect, useState, useRef} from 'react';
-import axios from 'axios';
+import {useState, useRef} from 'react';
+// import axios from 'axios';
 import './App.css';
 
 
 function App() {
   const [keyword, setKeyword] = useState<string>('Christmas');
   const [open, setOpen] = useState<boolean>(false);
-  // const [input, setinput] = useRef()
+  const kInput = useRef<any>(null);
+
   const handleKeyword = () => {
     setOpen(true)
+  }
+  const handleInput = () => {
+    if(kInput.current){
+      console.log(kInput)
+      setKeyword(kInput.current.value)
+      kInput.current.value = null;
+    }
   }
   // useEffect(()=>{
   //   axios.get()
@@ -17,9 +24,9 @@ function App() {
   return (
     <>
     <h3 className='App-header'>This is my successfully setup React App 💯🥇</h3>
-    <label className='kw-labl'> keyword: <input className='kw-input' value={keyword}/></label>
+    <label className='kw-labl'> keyword: <input className='kw-input' ref={kInput} value={keyword} onChange={handleInput}/></label>
     <button className='search-btn' type='button' onClick={handleKeyword}>search</button>
-    {open? <p>the key word already handled :alarm_clock :abacus:</p> : <></>}
+    {open? <p>the {keyword} already handled :alarm_clock :abacus:</p> : <></>}
     </>
   );
 }
